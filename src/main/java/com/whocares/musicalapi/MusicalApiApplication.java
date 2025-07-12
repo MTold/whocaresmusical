8103215f4750aaae2be4eed626bd6eec96217eb7
Harris Huang <1467154899@qq.com>
2025-07-12 19:40:26 +0800

    Initial commit

---
+++ src/main/java/com/whocares/musicalapi/MusicalApiApplication.java
@@ -0,0 +1,13 @@
+package com.whocares.musicalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whocares/musicalapi/MusicalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whocares.musicalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
