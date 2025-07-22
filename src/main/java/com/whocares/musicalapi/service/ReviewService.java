bceb1d1e5df251dce15b6c03ef08a00e5773b325
Pigeonnie <1363746469@qq.com>
2025-07-22 14:00:04 +0800

    注释掉getALL的方法和新增库包

--- src/main/java/com/whocares/musicalapi/service/ReviewService.java
+++ src/main/java/com/whocares/musicalapi/service/ReviewService.java
@@ -4,13 +4,19 @@ import com.whocares.musicalapi.dto.request.ReviewRequest;
 import com.whocares.musicalapi.dto.response.ReviewResponse;
 import com.whocares.musicalapi.dto.response.ReviewStatisticsResponse;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
 
 public interface ReviewService {
     
     Page<ReviewResponse> getReviewsByPerformance(Long performanceId, int page, int size, Integer rating);
     
     Page<ReviewResponse> getReviewsByUser(String username, int page, int size);
+
+    // 添加新的方法来按状态分类获取所有评价
+    //List<ReviewResponse> getReviewsByStatus(Integer status, Pageable pageable);
     
     ReviewStatisticsResponse getReviewStatistics(Long performanceId);
     
